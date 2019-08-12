31f720ca9d4920b8938a61a2878128c97cc7b046
John Collier <John.J.Collier@ibm.com>
2019-08-12 13:03:18 -0400

    Address review comments, add command to redeploy
    Signed-off-by: John Collier <John.J.Collier@ibm.com>

---
+++ codewind-che-sidecar/deploy-pfe/pkg/constants/defaults.go
@@ -0,0 +1,32 @@
+package constants
+
+import corev1 "k8s.io/api/core/v1"
+
+const (
+	// PFEPrefix is the prefix all PFE-related resources: deployment, service, and ingress/route
+	PFEPrefix         = "codewind"
+
+	// PerformancePrefix is the prefix for all performance-dashboard related resources: deployment and service
+	PerformancePrefix = PFEPrefix + "-performance"
+
+	// PFEImage is the docker image that will be used in the Codewind-PFE pod
+	PFEImage         = "eclipse/codewind-pfe-amd64"
+
+	// PerformanceImage is the docker image that will be used in the Performance dashboard pod
+	PerformanceImage = "eclipse/codewind-performance-amd64"
+
+	// PFEImageTag is the image tag associated with the docker image that's used for Codewind-PFE
+	PFEImageTag    = "latest"
+
+	// PerformanceTag is the image tag associated with the docker image that's used for the Performance dashboard
+	PerformanceTag = "latest"
+
+	// ImagePullPolicy is the pull policy used for all containers in Codewind, defaults to Always
+	ImagePullPolicy = corev1.PullAlways
+
+	// PFEContainerPort ais the port at which Codewind-PFE is exposed
+	PFEContainerPort         = 9191
+
+	// PerformanceContainerPort is the port at which the Performance dashboard is exposed
+	PerformanceContainerPort = 9095
+)
---
+++ codewind-che-sidecar/deploy-pfe/pkg/kube/kube.go
@@ -0,0 +1,54 @@
+package kube
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+// GetKubeClientConfig retrieves the Kubernetes client config from the cluster
+func GetKubeClientConfig() clientcmd.ClientConfig {
+	// Instantiate loader for kubeconfig file.
+	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		clientcmd.NewDefaultClientConfigLoadingRules(),
+		&clientcmd.ConfigOverrides{},
+	)
+	return kubeconfig
+}
+
+// GetCurrentNamespace gets the current namespace in the Kubernetes context
+func GetCurrentNamespace() string {
+	// Instantiate loader for kubeconfig file.
+	kubeconfig := GetKubeClientConfig()
+	namespace, _, err := kubeconfig.Namespace()
+	if err != nil {
+		panic(err)
+	}
+	return namespace
+}
+
+// DetectOpenShift3 determines if we're running on an OpenShift 3.x cluster
+// From https://github.com/eclipse/che-operator/blob/2f639261d8b5416b2934591e12925ee0935814dd/pkg/util/util.go#L63
+func DetectOpenShift3(config *rest.Config) bool {
+	discoveryClient, err := discovery.NewDiscoveryClientForConfig(config)
+	if err != nil {
+		log.Errorf("Unable to detect if running on OpenShift: %v\n", err)
+		os.Exit(1)
+	}
+	apiList, err := discoveryClient.ServerGroups()
+	if err != nil {
+		log.Errorf("Error attempting to retrieve list of API Groups: %v\n", err)
+		os.Exit(1)
+	}
+	apiGroups := apiList.Groups
+	for _, group := range apiGroups {
+		if group.Name == "route.openshift.io" {
+			return true
+		}
+	}
+	return false
+}
